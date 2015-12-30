d10b26face970fe94ce1b56203ba0069de9efa6d
beerlington <pete@lette.us>
2015-12-30 10:50:41 -0500

    Adds has many votes relationship to proposals

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -4,6 +4,7 @@ class Proposal < ActiveRecord::Base
   validates :abstract, presence: true
 
   belongs_to :user
+  has_many :votes
   accepts_nested_attributes_for :user, update_only: true
 
   delegate :email, :name, :bio, to: :user, prefix: true
