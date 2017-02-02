70610155fddbc1d78de5309b5c5f85d9883c67b9
beerlington <pete@lette.us>
2017-02-01 20:21:56 -0500

    Updates cutoff date to 2017

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Proposals
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
 
-    config.cutoff_date = "January 30th, 2016".to_datetime + 4.hours
+    config.cutoff_date = "March 15th, 2017".to_datetime + 4.hours
   end
 end
